01cfebc0a7511497c0736c2b1d5d990a30249d38
Awele1111 <awelelan@Aweles-MacBook-Air.local>
2023-04-07 08:56:54 -0500

    start apollo to server added

--- server/server.js
+++ server/server.js
@@ -17,9 +17,16 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-});
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
 
 startApolloServer(typeDefs, resolvers);
